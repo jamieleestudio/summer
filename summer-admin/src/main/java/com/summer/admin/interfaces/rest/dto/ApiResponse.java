5adee8b7a3fa829dc4aeddcb03cb578bd9a63d02
jamielee <jamielee.studio@gmail.com>
2025-11-21 17:14:33 +0800

    feat: add  api response

---
+++ summer-admin/src/main/java/com/summer/admin/interfaces/rest/dto/ApiResponse.java
@@ -0,0 +1,43 @@
+package com.summer.admin.interfaces.rest.dto;
+
+public class ApiResponse<T> {
+    private String code;
+    private String message;
+    private T data;
+
+    public ApiResponse() {}
+
+    public ApiResponse(String code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>("OK", "success", data);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
