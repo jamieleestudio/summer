25bde6874b797dcd7ae2d3c7b887d6ba3082f6a1
jamielee <jamielee.studio@gmail.com>
2025-11-29 00:37:21 +0800

    chore: do something

---
+++ summer-iam/src/main/java/com/summer/iam/interfaces/rest/dto/role/RoleDetailResponse.java
@@ -0,0 +1,26 @@
+package com.summer.iam.interfaces.rest.dto.role;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoleDetailResponse {
+    private String id;
+    private String name;
+    private String description;
+    private Integer permissionScope;
+    private Integer sort;
+    private List<String> permissions = new ArrayList<>();
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public Integer getPermissionScope() { return permissionScope; }
+    public void setPermissionScope(Integer permissionScope) { this.permissionScope = permissionScope; }
+    public Integer getSort() { return sort; }
+    public void setSort(Integer sort) { this.sort = sort; }
+    public List<String> getPermissions() { return permissions; }
+    public void setPermissions(List<String> permissions) { this.permissions = permissions; }
+}
